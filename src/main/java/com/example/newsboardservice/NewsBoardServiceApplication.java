a6064952ac772e39a05ff5fdc176198fe8b37f75
jiesus815 <jihj815@naver.com>
2024-12-18 17:01:30 +0900

    내 브렌치 커밋

---
+++ src/main/java/com/example/newsboardservice/NewsBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsBoardServiceApplication.class, args);
+    }
+
+}
